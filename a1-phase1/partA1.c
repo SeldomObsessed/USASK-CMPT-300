3929d69657cf101ac9f8a866af5b71dcd4174cd4
William Morris <wjm625@usask.ca>
2025-09-11 10:02:12 -0600

    Input parser almost implemented

--- a1-phase1/partA1.c
+++ a1-phase1/partA1.c
@@ -12,11 +12,57 @@
  *
  * At this point, only skeletons with no full implementation will be present.
  * 
- * - caller responsible for valid input, but in this case callee will 
- *   perform input validation.
+ * - caller responsible for valid input, but also callee will perform input 
+ *   validation.
  * /
  
 
+#include <square.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <windows.h>
+#include <tchar.h>
+#include strsafe.h>
 
+/*TODO update after testing*/
+#define MAX_THREADS 1024 
 
+DWORD WINAPI MyThreadsFunction( LPVOID lpParam);
+void ErrorHandler(LPCSTR lpszFunction); 
 
+/*
+ * Main entry point.
+ *
+ * Will take 3 parameters, only accepts integers: 
+ *   1: no. of threads to spawn
+ *   2: length of deadline
+ *   3: parameter passed to square()
+ */
+
+int main void(int argc, char *argv[])
+{
+  if (argc < 3)
+  {
+    printf("Usage: expecting 3 parameters\n");
+    exit(1);
+  }
+
+  int args[argc-1];
+  
+  /*Converts args to integers*/
+  for (int i =1; i<argc; i++)
+  {
+    if !(is_valid(argv[i]))
+    /*TODO write int is_valid(int x) in square.c and add sign to square.h*/
+    {
+      fprintf(stderr, "Error: argument '%s' is not an integer\n", argv[i]);
+    }
+    args[i-1] = atoi(argv[i]);
+  }
+
+  /*args[] contain parsed integers*/
+
+  /*TODO write function calls*/  
+  
+}
