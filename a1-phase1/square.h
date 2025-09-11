fbf9d080766956c011624da467549377a39bb3ce
William Morris <wjm625@usask.ca>
2025-09-11 13:01:36 -0600

    Adding square.h

---
+++ a1-phase1/square.h
@@ -0,0 +1,12 @@
+/*
+ * filename.(c/h)
+ * Logan Fossenier & William Morris
+ * hzv143 & wjm625
+ * 11343891 & 11278140
+ * CMPT332 Fall 2025
+ */
+
+
+int Square(int);
+int is_valid(char *arg);  
+
