cbd88439defbae8fc566f80bdd14017864194e87
Logan Fossenier <hzv143@usask.ca>
2025-09-13 19:13:26 -0600

    feat: write ListCreate test
    mytestlist now functions (not complete)

--- a1-phase1/mytestlist.c
+++ a1-phase1/mytestlist.c
@@ -6,13 +6,29 @@
  * CMPT332 Fall 2025
  */
 
+#include <stdio.h>
+
+#include <list.h>
 
 int main(int argc, char **argv)
 {
+  int result, item;
+  LIST *my_list;
+  LIST local_list;
+
   /* dispell compiler complaints */
   argc++;
   argv[0][0] = 'a';
 
+  my_list  = ListCreate();
+  my_list = &local_list;
+  item = 42;
+
+  result = ListAdd(my_list, &item);
+  if (result == 0)
+  {
+    printf("Hey that worked \n");
+  }
   return 0;
 }
 
