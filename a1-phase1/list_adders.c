09169cef37004b8ab5d21b56e192648a5b8b9277
Logan Fossenier <hzv143@usask.ca>
2025-09-14 12:14:40 -0600

    feat: add documentation and remove helpers

--- a1-phase1/list_adders.c
+++ a1-phase1/list_adders.c
@@ -10,13 +10,30 @@
 
 #include <list.h>
 
-
+/*
+ * makes a new list and returns the reference to the user for later access in
+ * the API
+ *
+ * tags the pointer for internal handling, making the pointer potentially
+ * invalid when dereferenced outside of the API
+ *
+ * returns a pointer to a new empty LIST
+ */
 LIST *ListCreate()
 {
   return NULL;
 }
 
-
+/*
+ * adds an element to the LIST after the "current" position in the LIST and the
+ * new element becomes the "current" position
+ *
+ * LIST *list: the LIST which will be added onto
+ * void *item: the item to be added
+ *
+ * returns 0 on success
+ * returs -1 on failure
+ */
 int ListAdd(LIST *list, void *item)
 {
   /* check that correct type and range of parameter values have been passed */
