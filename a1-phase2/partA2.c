2219d3696bea99db50f55fa4cd662fd9a9daf69d
William Morris <wjm625@usask.ca>
2025-09-17 14:26:58 -0600

    starting UBC threads

---
+++ a1-phase2/partA2.c
@@ -0,0 +1,31 @@
+/*                                                                              
+ * partA1.c                                                                     
+ * Logan Fossenier & William Morris                                             
+ * hzv143 & wjm625                                                              
+ * 11343891 & 11278140                                                          
+ * CMPT332 Fall 2025                                                            
+ *                                                                              
+ * This file implements the main executable for A.1. This executable creates    
+ * multiple threads which perform the same action until a deadline, without     
+ * synchronization between processes/threads. number of threads, length of      
+ * deadline, and maximum integer size are to be command-line parameters.        
+ *                                                                              
+ * At this point, only skeletons with no full implementation will be present.   
+ *                                                                              
+ * - caller responsible for valid input, but also callee will perform input     
+ *   validation.                                                                
+ */ 
+
+#include <square.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <rtthreads.h>
+
+void invoke_square(int size);
+
+
+
+int main(int argc, char* argv)
+{
+  
+}
