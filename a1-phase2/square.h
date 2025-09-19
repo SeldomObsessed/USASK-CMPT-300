a828db0561e10e80c99aacbf54a0050c265ca4bb
William Morris <wjm625@usask.ca>
2025-09-19 13:47:00 -0600

    unchanged square files

---
+++ a1-phase2/square.h
@@ -0,0 +1,17 @@
+/*
+ * filename.(c/h)
+ * Logan Fossenier & William Morris
+ * hzv143 & wjm625
+ * 11343891 & 11278140
+ * CMPT332 Fall 2025
+ */
+
+
+#ifndef SQUARE_H
+#define SQUARE_H
+
+extern square_counter;
+int Square(int);
+int is_valid(char*);  
+
+#endif
